6ea6c7c9c0523e305fc92740e0948e91884ea504
Abdullah <abd.247@hotmail.com>
2023-09-24 14:19:40 -0400

    Initial Commit

---
+++ Diabud-iOS/Diabud_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diabud_iOSApp.swift
+//  Diabud-iOS
+//
+//  Created by Abdullah Shahid on 2023-09-24.
+//
+
+import SwiftUI
+
+@main
+struct Diabud_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
